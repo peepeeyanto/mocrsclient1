8418675ae3421e9674e01b57d83bb30f0958ae7b
unknown <doni.febrian20@mhs.uinjkt.ac.id>
2023-06-19 21:12:28 +0700

    fixes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 import json
 import cv2
-
+import time
 
 def main():
-    api_url = "http://127.0.0.1:5000/rslogin"
+    api_url = "YOUR_RSLOGIN_ENDPOINT"
 
-    hospitalID = input("enter your hospital ID: ")
-    password = input("enter your password: ")
+    hospitalID = "YOUR_HOSPITAL_ID"
+    password = "YOUR_HOSPITAL_PASSWORD"
     todo = {"hospitalID": hospitalID, "password": password}
     headers =  {"Content-Type":"application/json"}
     response = requests.post(api_url, data=json.dumps(todo), headers=headers)
@@ -17,19 +17,20 @@ def main():
     
     data = response.json()
     token = data['token']
-    streamlink = input("enter your rtsp link: ")
+    streamlink = "YOUR_RSTP_LINK"
 
-    cap = cv2.VideoCapture(streamlink)
+    
 
     while True:
+        cap = cv2.VideoCapture(streamlink)
         # Read a frame from the stream
         ret, frame = cap.read()
 
         # Save the frame as a screenshot
-        cv2.imwrite('screenshot.png', frame)
+        cv2.imwrite('screenshot.jpg', frame)
 
         # Send request to REST API
-        url = 'http://127.0.0.1:5000/ml'
+        url = 'YOUR_ML_ENDPOINT'
         files = {'file': ("screenshot.jpg", open('screenshot.jpg', 'rb'), "image/jpg")}
         headers1 = {"authorization": token}
         response = requests.post(url, files=files, headers=headers1)
@@ -38,6 +39,6 @@ def main():
         print(response.text)
 
         # Wait for 10 minutes before capturing the next screenshot
-        cv2.waitKey(60000)  # 600,000 milliseconds = 10 minutes
+        time.sleep(600)
         
 main()
